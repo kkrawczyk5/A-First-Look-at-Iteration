5fa6b2d7fbc5cf9a96e5966fc5b8223e42aa17cf
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-16 18:30:43 -0600

    Update Chapter 5 Problem 3.py

--- Chapter 5 Problem 3.py
+++ Chapter 5 Problem 3.py
@@ -14,7 +14,8 @@ length = int(input("How long will each line be? - "))
 line_color = input("What should the color of the line be? - ")
 fill_color = input("What color should the inside of the polygon be? - ")
 # asks the user for the color of both the line and the inside fill of the polygon
-
+#Add two lines to correct the fill color requirement
+#jimmy.begin_fill()
 jimmy.color(line_color)
 jimmy.fillcolor(fill_color)
 # function that changes the color of the line and the fill depending on the user input
@@ -22,5 +23,6 @@ jimmy.fillcolor(fill_color)
 for shape in range(int(sides)):
   jimmy.forward (int(length))
   jimmy.right (int(360/sides))
+#jimmy.end_fill()
 
 #for-loop that takes the user input and creates the shape
